a32af4107377b79b35b352b8db6edb7d93f72c0b
phantom820 <1611821@students.wits.ac.za>
2020-12-04 00:45:23 +0200

    fixing ci

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -12,3 +12,4 @@ def test_optimization_problem():
 		assert optimization_problem.upper_constraints == np.array([6])
 		random_input = np.random.randn(2,2)
 		assert optimization_problem.objective_function(random_input)[0] == eval('lambda x:x[:,0]**2-4*x[:,0]+4')(random_input)[0]
+		assert optimization_problem.objective_function(random_input)[1] == eval('lambda x:x[:,0]**2-4*x[:,0]+4')(random_input)[1]
